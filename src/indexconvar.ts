405ddd9d6b1dbe834b950ec0847333ff6860ec31
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-11 10:52:38 -0300

    Initial commit

---
+++ src/indexconvar.ts
@@ -0,0 +1,85 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+/* 
+console.log("SECUENCIA GRANJERA 1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("giroizq90");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+
+console.log("SECUENCIA GRANJERA 2");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("avanza1");
+console.log("rellena1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+console.log("dig1");
+console.log("avanza1");
+
+console.log("SECUENCIA GRANJERA 3");
+console.log("giraizq90");
+console.log("avanza1");
+console.log("girader90");
+console.log("avanza1");
+console.log("avanza1");
+console.log("girader90");
+console.log("avanza1");
+console.log("rellena1");
+*/
+
+
+console.log("SECUENCIA GRANJERA CON VARIABLE 1");
+
+var kk = [
+ 
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "gira izq90"
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "avanza1, dig1"
+  "avanza1, dig1"
+
+]
+
+for (let i=0; i<kk.length; i++) {
+  let granjera1 = (kk[i])
+
+  console.log (granjera1);
+
+}
+
+
+
+
+
+
